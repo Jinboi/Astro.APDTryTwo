6315c9da4a4f510410053165377a75e213ffef49
Jinboi <duddnwkfgo7@gmail.com>
2024-12-30 15:32:11 -0800

    added ssr and deplyed to netlify

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,1 +1,4 @@
-export default new Map();
+
+export default new Map([
+["src/content/posts/behind-the-scenes-with-our-artists.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fbehind-the-scenes-with-our-artists.mdx&astroContentModuleFlag=true")]]);
+		
